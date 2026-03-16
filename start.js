58e57d130f0ab54a448db38519d607e02931c9c7
mehtamridul0909 <mridulmehta444op@gmail.com>
2026-03-16 17:07:50 +0530

    update

--- start.js
+++ start.js
@@ -1,8 +1,27 @@
-const Student = {
-    fullName : "Mridul Mehta",
-    age : 20,
-    cgpa : 7.61,
-    isPass : true
+// const Student = {
+//     fullName : "Mridul Mehta",
+//     age : 20,
+//     cgpa : 7.61,
+//     isPass : true
+// }
+
+// console.log(Student["age"]);
+// const product = {
+//     name : "Parker jotter standard CT ball pen",
+//     ink_color : "Black",
+//     rating : 4,
+//     status : "Deal of the Day",
+//     Price : 270
+// }
+// console.log(product["rating"]);
+
+const Profile = {
+    name : "Mridul Mehta",
+    post : 0,
+    followers : 62,
+    following : 70,
+    role : "Software Developer",
+    Bio : "LPU | Ex-Innocenty, In the world of algorithm i choose to stay a Human"
 }
 
-console.log(Student["age"]);
+console.log(Profile["followers"]);
